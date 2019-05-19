30116d27088cfc2e9a188758b859d4c7943ccf17
Berta Turcotte <bturcott@vo-f2.21-school.ru>
2019-05-19 20:23:57 +0300

    parser++

--- srcs/parser.c
+++ srcs/parser.c
@@ -6,7 +6,7 @@
 /*   By: bturcott <bturcott@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/19 15:49:37 by bturcott          #+#    #+#             */
-/*   Updated: 2019/05/19 19:54:22 by bturcott         ###   ########.fr       */
+/*   Updated: 2019/05/19 20:22:10 by bturcott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,12 +42,13 @@ char *parse_key(char *file, int pos)
 			}
 		pos++;
 	}
+	return (NULL);
 }
 
 void *parse_value(char *file, int pos)
 {
-	while (file[pos] && file[pos] != '"' || file[pos] != '[' 
-	|| file[pos] != '{' || !ft_isdigit(file[pos]))
+	while (file[pos] && (file[pos] != '"' || file[pos] != '[' 
+	|| file[pos] != '{' || !ft_isdigit(file[pos])))
 		pos++;
 	if (file[pos] == '"')
 		return (parse_key(file, pos));
@@ -55,8 +56,10 @@ void *parse_value(char *file, int pos)
 		return NULL;
 	else if (ft_isdigit(file[pos]))
 		return(ft_atoi(file + pos));
-	
+	else
+		return (NULL);
 }
+
 t_json *make_json(char *file, int pos)
 {
 	t_json	*obj;
@@ -66,14 +69,18 @@ t_json *make_json(char *file, int pos)
 	i = -1;
 	while (file[pos] && ++i < MAX_FIELDS)
 	{
+		printf("2\n");
 		obj->key[i] = parse_key(file, pos);
 		while (file[pos] != ':')
 			pos++;
+		printf("3\n");
 		if (!(obj->value[i] = parse_value(file, pos)))
 			obj->value[i] = make_json(file, file - ft_strchr(file + pos, '{') + 1);
-		while (file[pos] != ',')
-			pos++;
+		while (file[pos++] != ',')
+			;
+		printf("3\n");
 	}
+	return (obj);
 }
 
 char *read_file(int fd){
@@ -83,7 +90,7 @@ char *read_file(int fd){
 	int i;
 	
 	i = 0;
-	while ((temp = get_next_line(fd, &temp)))
+	while (get_next_line(fd, &temp))
 	{
 		file = ft_strjoin(file, temp);
 		free(temp);
@@ -103,7 +110,7 @@ t_list *parse_json(char *config_file)
 	t_list *tmp;
 	char 	*file;
 
-	fd = fopen(config_file, O_RDONLY);
+	fd = open(config_file, O_RDONLY);
 	if (fd == -1 || fd == 0)
 	{   
 		ft_putendl("Missing config file...");
@@ -111,10 +118,18 @@ t_list *parse_json(char *config_file)
 	}
 	file = read_file(fd);
 	close(fd);
-	while (obj = make_json(file, 0))
+	printf("1\n");
+	while ((obj = make_json(file, 0)))
 	{
 		tmp = ft_lstnew((void *)obj, sizeof(obj));
 		ft_lstpushback(json, tmp);
 	}
 	return (json);
+}
+
+
+int main(int ac, char **av)
+{
+	printf("%s\n", av[1]);
+	t_list *json = parse_json(av[1]);
 }
