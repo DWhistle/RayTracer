3d3b4ab25f0dcb03b6ef550dc98cd2cbc9381d7a
Berta Turcotte <bturcott@vo-f2.21-school.ru>
2019-05-20 17:30:48 +0300

    proceeds simple jsons

--- srcs/parser.c
+++ srcs/parser.c
@@ -6,7 +6,7 @@
 /*   By: bturcott <bturcott@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/19 15:49:37 by bturcott          #+#    #+#             */
-/*   Updated: 2019/05/19 20:22:10 by bturcott         ###   ########.fr       */
+/*   Updated: 2019/05/20 17:10:02 by bturcott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,14 +31,17 @@ char *parse_key(char *file, int pos)
 {
 	int quotes;
 	
-	quotes = pos;
+	quotes = 0;
 	while (file[pos])
 	{
-		if (file[pos++] == '"')
+		if (file[pos] == '"')
 			{
-				while (file[pos] && file[pos] != '"' && file[pos - 1] != '\\')
-					pos++;
-				return (ft_strsub(file, quotes, pos));
+				++pos;
+				printf("%d\n", pos);
+				while (file[pos + quotes] && file[pos + quotes] != '"')
+					quotes++;
+				printf("quotes %d pos == %d\n", quotes, pos);
+				return (ft_strsub(file, pos, quotes));
 			}
 		pos++;
 	}
@@ -47,40 +50,46 @@ char *parse_key(char *file, int pos)
 
 void *parse_value(char *file, int pos)
 {
-	while (file[pos] && (file[pos] != '"' || file[pos] != '[' 
-	|| file[pos] != '{' || !ft_isdigit(file[pos])))
+	while (file[pos] && file[pos] != '"' && file[pos] != '[' 
+	&& file[pos] != '{' && !ft_isdigit(file[pos]))
 		pos++;
+	printf("%c\n", file[pos + 1]);
 	if (file[pos] == '"')
 		return (parse_key(file, pos));
-	else if (file[pos++] == '{')
-		return NULL;
 	else if (ft_isdigit(file[pos]))
 		return(ft_atoi(file + pos));
 	else
 		return (NULL);
 }
 
-t_json *make_json(char *file, int pos)
+t_json *make_json(char *file, int pos, t_json *obj)
 {
-	t_json	*obj;
 	int		i;
-
+	
+	if (file[pos] == '}')
+		return NULL;
 	obj = create_json_obj();
 	i = -1;
-	while (file[pos] && ++i < MAX_FIELDS)
+	while (file[pos] && file[pos] != '}' && ++i < MAX_FIELDS)
 	{
-		printf("2\n");
+		printf("position before key %d\n", pos);
 		obj->key[i] = parse_key(file, pos);
-		while (file[pos] != ':')
+		printf("key == %s     pos == %d\n", obj->key[i], pos);
+		while (file[pos] && file[pos] != ':')
 			pos++;
-		printf("3\n");
-		if (!(obj->value[i] = parse_value(file, pos)))
-			obj->value[i] = make_json(file, file - ft_strchr(file + pos, '{') + 1);
-		while (file[pos++] != ',')
-			;
-		printf("3\n");
+		obj->value[i] = parse_value(file, ++pos);
+		while (file[pos] && file[pos] != ',')
+			pos++;
+		printf("val == %d     pos == %d\n", obj->value[i], pos);	
+		while (file[pos] && file[pos] != '"' && file[pos] != '}')
+			pos++;
+		if (file[pos] == '}')
+			return (pos + 1);
+		else if (file[pos] == 0)
+			return (0);
+		printf("%c\n", file[pos]);
 	}
-	return (obj);
+	return (pos);
 }
 
 char *read_file(int fd){
@@ -107,10 +116,11 @@ t_list *parse_json(char *config_file)
 	int fd;
 	t_list *json;
 	t_json *obj;
-	t_list *tmp;
+	int pos;
 	char 	*file;
 
 	fd = open(config_file, O_RDONLY);
+	pos = 0;
 	if (fd == -1 || fd == 0)
 	{   
 		ft_putendl("Missing config file...");
@@ -119,10 +129,10 @@ t_list *parse_json(char *config_file)
 	file = read_file(fd);
 	close(fd);
 	printf("1\n");
-	while ((obj = make_json(file, 0)))
+	while ((pos = make_json(file, pos, obj)))
 	{
-		tmp = ft_lstnew((void *)obj, sizeof(obj));
-		ft_lstpushback(json, tmp);
+		ft_lstpushback(json, ft_lstnew((void *)obj, sizeof(obj)));
+		printf("list");
 	}
 	return (json);
 }
