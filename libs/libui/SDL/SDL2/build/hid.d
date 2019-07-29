0b5cec5090612b2909e4295dd2cb0dc40126a4c6
hgreenfe <hgreenfe@student.21-school.ru>
2019-07-29 21:58:07 +0300

    удалил лишние файлы

--- libs/libui/SDL/SDL2/build/hid.d
+++
@@ -1,8 +0,0 @@
-build/hid.lo: \
-  /Volumes/Storage/goinfre/kmeera-r/RayTracer/libs/libui/SDL/SDL2/src/hidapi/mac/hid.c \
-  /Volumes/Storage/goinfre/kmeera-r/RayTracer/libs/libui/SDL/SDL2/src/hidapi/mac/../../SDL_internal.h \
-  /Volumes/Storage/goinfre/kmeera-r/RayTracer/libs/libui/SDL/SDL2/src/hidapi/mac/../../dynapi/SDL_dynapi.h \
-  /Volumes/Storage/goinfre/kmeera-r/RayTracer/libs/libui/SDL/SDL2/src/hidapi/mac/../../dynapi/SDL_dynapi_overrides.h \
-  include/SDL_config.h include/SDL_platform.h include/begin_code.h \
-  include/close_code.h \
-  /Volumes/Storage/goinfre/kmeera-r/RayTracer/libs/libui/SDL/SDL2/src/hidapi/hidapi/hidapi.h
--- libs/libui/SDL/SDL2/sdl2-config.cmake
+++ libs/libui/SDL/SDL2/sdl2-config.cmake
@@ -1,10 +1,10 @@
 # sdl2 cmake project-config input for ./configure scripts
 
-set(prefix "/Volumes/Storage/goinfre/kmeera-r/RayTracer/libs/libui/SDL") 
-set(exec_prefix "${prefix}")
+set(prefix "/Users/hgreenfe/main_learn/rt/RT/libs/libui/SDL") 
+set(exec_prefix "/Users/hgreenfe/main_learn/rt/RT/libs/libui/SDL")
 set(libdir "${exec_prefix}/lib")
-set(SDL2_PREFIX "/Volumes/Storage/goinfre/kmeera-r/RayTracer/libs/libui/SDL")
-set(SDL2_EXEC_PREFIX "/Volumes/Storage/goinfre/kmeera-r/RayTracer/libs/libui/SDL")
+set(SDL2_PREFIX "/Users/hgreenfe/main_learn/rt/RT/libs/libui/SDL")
+set(SDL2_EXEC_PREFIX "/Users/hgreenfe/main_learn/rt/RT/libs/libui/SDL")
 set(SDL2_LIBDIR "${exec_prefix}/lib")
 set(SDL2_INCLUDE_DIRS "${prefix}/include/SDL2")
 set(SDL2_LIBRARIES "-L${SDL2_LIBDIR}  -lSDL2")
