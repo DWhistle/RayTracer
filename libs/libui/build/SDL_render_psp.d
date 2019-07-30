136239e2fd02ee82217e58d1bf4fac66aea1f7c8
Kodlak Meera reed <kmeera-r@at-p1.21-school.ru>
2019-07-30 11:23:54 +0300

    her

--- libs/libui/build/SDL_render_psp.d
+++
@@ -1,9 +0,0 @@
-build/SDL_render_psp.lo: \
-  /Volumes/Storage/goinfre/kmeera-r/RayTracer/libs/libui/SDL/SDL2/src/render/psp/SDL_render_psp.c \
-  /Volumes/Storage/goinfre/kmeera-r/RayTracer/libs/libui/SDL/SDL2/src/render/psp/../../SDL_internal.h \
-  /Volumes/Storage/goinfre/kmeera-r/RayTracer/libs/libui/SDL/SDL2/src/render/psp/../../dynapi/SDL_dynapi.h \
-  /Volumes/Storage/goinfre/kmeera-r/RayTracer/libs/libui/SDL/SDL2/src/render/psp/../../dynapi/SDL_dynapi_overrides.h \
-  include/SDL_config.h \
-  /Volumes/Storage/goinfre/kmeera-r/RayTracer/libs/libui/SDL/SDL2/include/SDL_platform.h \
-  /Volumes/Storage/goinfre/kmeera-r/RayTracer/libs/libui/SDL/SDL2/include/begin_code.h \
-  /Volumes/Storage/goinfre/kmeera-r/RayTracer/libs/libui/SDL/SDL2/include/close_code.h
