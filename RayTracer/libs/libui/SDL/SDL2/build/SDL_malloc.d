874b62c5becb8f7f87c022162ea5ddbef9fde11c
hgreenfe <hgreenfe@student.21-school.ru>
2019-10-16 20:45:34 +0300

    obj

--- RayTracer/libs/libui/SDL/SDL2/build/SDL_malloc.d
+++
@@ -1,8 +0,0 @@
-build/SDL_malloc.lo: \
-  /Users/hgreenfe/main_learn/rtv1/test/rtv1/libs/libui/SDL/SDL2/src/stdlib/SDL_malloc.c \
-  /Users/hgreenfe/main_learn/rtv1/test/rtv1/libs/libui/SDL/SDL2/src/stdlib/../SDL_internal.h \
-  /Users/hgreenfe/main_learn/rtv1/test/rtv1/libs/libui/SDL/SDL2/src/stdlib/../dynapi/SDL_dynapi.h \
-  /Users/hgreenfe/main_learn/rtv1/test/rtv1/libs/libui/SDL/SDL2/src/stdlib/../dynapi/SDL_dynapi_overrides.h \
-  include/SDL_config.h include/SDL_platform.h include/begin_code.h \
-  include/close_code.h include/SDL_stdinc.h include/SDL_atomic.h \
-  include/SDL_error.h
